f6cb9ca4376306e27952de73e2f430242106af55
Olivia Newton <obn001@morningside.edu>
2026-05-27 01:28:48 -0500

    Add lib folder and supabase file

---
+++ buildtrack/lib/supabase.ts
@@ -0,0 +1,20 @@
+/*import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
+*/
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY as string;
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+);
